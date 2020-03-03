186e05e3db6f1f4d743867325532dbffd191d387
eowang <31154739+eowang@users.noreply.github.com>
2020-03-02 17:27:56 -0800

    Update milestone2.py
    Update

--- milestone2.py
+++ milestone2.py
@@ -1,26 +1,39 @@
 import pandas as pd
 import sys
 import matplotlib.pyplot as plt 
+import numpy as np
 
-def nearestSubway(data):
+def Min_Euclidean_Dist(data1, data2, cols=['latitude','longitude']):
+    data2['distance']= data2.apply(lambda x: np.square())
+    return np.linalg.norm(data1[cols].values - data2[cols].values,
+                   axis=1)
+    
+
+def Nearest_Subway(data):
     subwayData= pd.read_csv("subwayData.csv")
     # subwayData['coordinates'] = subwayData['the_geom'].str.split(' (').str[0]
+
+
     subwayData['longitude']= subwayData['the_geom'].apply(lambda st: st[st.find("(")+1:st.find("4")])
     subwayData['latitude']= subwayData['the_geom'].apply(lambda st: st[st.find(" 4")+1:st.find(")")])
+    
+    distanceArray= Min_Euclidean_Dist(data,subwayData)
+    data.apply(lambda x: Min_Euclidean_Dist())
 
-    subwayData.to_csv('subwayData1.csv', index=False)
 
-def Feature_Selection(data):
+# def Feature_Selection(data):
 
-def Decision_Tree(data):
+# def Decision_Tree(data):
 
-def Logistic_Regression(data):
+# def Logistic_Regression(data):
 
-def SVM(data):
+# def SVM(data):
 
 if __name__ == '__main__':
-	data = pd.read_json(sys.argv[1])
-	Feature_Selection(data)
-	Decision_Tree(data)
-	Logistic_Regression(data)
-	SVM(data)
+    data = pd.read_csv('raw_data.csv')
+	# data = pd.read_json(sys.argv[1])
+	# Feature_Selection(data)
+	# Decision_Tree(data)
+	# Logistic_Regression(data)
+	# SVM(data)
+    Nearest_Subway(data)
