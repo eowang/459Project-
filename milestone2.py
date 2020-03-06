6a192bce8f21645d76e6acc92249aff2d3171c82
eowang <31154739+eowang@users.noreply.github.com>
2020-03-05 19:48:45 -0800

    Update milestone2
    fixed

--- milestone2.py
+++ milestone2.py
@@ -3,6 +3,7 @@ import sys
 import matplotlib.pyplot as plt 
 import numpy as np
 import statistics
+from collections import Counter
 from sklearn.svm import SVC
 from sklearn.model_selection import KFold
 from sklearn.linear_model import LogisticRegression
@@ -57,34 +58,31 @@ def recursiveFeature(X,y):
     print("Feature Ranking: %s" % fit.ranking_)
 
 def Feature_Selection(data):
-    data = data.drop(data.columns[[2,3,4,5,6,10,11,13,16]], axis=1)
-    
-    cols = list(data)
-    cols.insert(-1, cols.pop(cols.index('interest_level')))
-    # cols.insert(-1, cols.pop(cols.index('dist_dt')))
-    data = data[cols]
-    data['longitude']= data['longitude'].abs()
-    data['Roof Deck']= data['Roof Deck'] | data['Roof-deck']
-    data['High Ceiling']= data['High Ceiling'] | data['High Ceilings'] |data['HIGH CEILINGS']
-    data['Renovated'] = data['Renovated'] | data['Newly renovated']
-    data['Live In Super'] = data['Live In Super'] | data['LIVE IN SUPER']
-    data['Gym/Fitness'] = data['Gym/Fitness'] | data['Fitness Center']
-    data.Pool = data.Pool | data['Swimming Pool']
+    # data['longitude']= data['longitude'].abs()
+    # data['Roof Deck']= data['Roof Deck'] | data['Roof-deck']
+    # data['High Ceiling']= data['High Ceiling'] | data['High Ceilings'] |data['HIGH CEILINGS']
+    # data['Renovated'] = data['Renovated'] | data['Newly renovated']
+    # data['Live In Super'] = data['Live In Super'] | data['LIVE IN SUPER']
+    # data['Gym/Fitness'] = data['Gym/Fitness'] | data['Fitness Center']
+    # data.Pool = data.Pool | data['Swimming Pool']
     data ['Hardwood'] = data.Hardwood | data.HARDWOOD | data['Hardwood Floors']
-    data['Outdoor Space'] = data['Outdoor Space'] | data['Common Outdoor Space'] | data['Private Outdoor Space'] | data['PublicOutdoor']
-    data['Elevator'] = data['elevator'] | data['Elevator'] 
+    # data['Outdoor Space'] = data['Outdoor Space'] | data['Common Outdoor Space'] | data['Private Outdoor Space'] | data['PublicOutdoor']
+    # data['Elevator'] = data['elevator'] | data['Elevator'] 
     data['LAUNDRY']= data['Laundry in Building'] | data['Laundry in Unit'] | data['Laundry In Building'] | data['Laundry Room'] | data['Laundry In Unit'] | data['LAUNDRY'] | data['On-site laundry'] | data['On-site Laundry'] | data['Washer/Dryer']
     data['Pre-War'] = data['Pre-War'] | data['prewar'] | data['Prewar']
-    data['Pets on approval']= data['Pets on approval'] | data['Dogs Allowed'] | data['Cats Allowed']
-    data.dishwasher = data.Dishwasher | data.dishwasher
+    # data['Pets on approval']= data['Pets on approval'] | data['Dogs Allowed'] | data['Cats Allowed']
+    # data.dishwasher = data.Dishwasher | data.dishwasher
     data.Garage = data.Garage | data['On-site Garage']
     data.drop(['Dogs Allowed','Cats Allowed','LIVE IN SUPER','Newly renovated','Washer/Dryer','HIGH CEILINGS','High Ceilings','Swimming Pool','Roof-deck','On-site Garage','Common Outdoor Space','Private Outdoor Space', 'PublicOutdoor','elevator','HARDWOOD', 'Hardwood Floors', 'Laundry in Building','Laundry in Unit','Laundry In Building','Laundry Room','Laundry In Unit', 'On-site laundry', 'On-site Laundry','prewar','Prewar','Dishwasher'], axis = 1, inplace=True) 
     
+  
+
     # X = data.drop['interest_level']  #independent columns
     # y = data['interest_level']   #target column i.e price range
     # data.to_csv('test_data.csv', index=False)
   
     # recursiveFeature(X,y)
+
 
 def Decision_Tree(X, y):
     log_scores = []
@@ -92,7 +90,7 @@ def Decision_Tree(X, y):
 
     kf = KFold(n_splits = 5)
 
-    dec_Tree_class = DecisionTreeClassifier(criterion='entropy')
+    dec_Tree_class = DecisionTreeClassifier(criterion='entropy', max_depth=5)
 
     for train_index, test_index in kf.split(X):
         X_train, X_valid = X.iloc[train_index], X.iloc[test_index]
@@ -112,19 +110,22 @@ def Decision_Tree(X, y):
 
 
 def Decision_tree_kaggle(test_data, X, y, listing_id):
-    tree_model = DecisionTreeClassifier(criterion='entropy')
+    tree_model = DecisionTreeClassifier(criterion='entropy', max_depth=5)
     tree_model.fit(X, y)
 
     predicted = tree_model.predict_proba(test_data)
 
     col_names = tree_model.classes_
     df_predict_proba = pd.DataFrame(predicted, columns= col_names)
+    df_predict_proba= df_predict_proba.reset_index(drop=True)
 
-    df_predict_proba['listing_id'] = listing_id.astype(int)
+    listing_id= listing_id.reset_index(drop=True)
+
+    df_predict_proba['listing_id'] = listing_id
     col_names = ['listing_id', 'high', 'medium', 'low']
 
     df_predict_proba = df_predict_proba[col_names]
-    df_predict_proba= df_predict_proba.dropna()
+    
     df_predict_proba.to_csv('kaggle_submission_TREE.csv', index=False)
 
 
@@ -143,7 +144,7 @@ def Logistic_Regression_score(X, y):
         logistic_model = LogisticRegression()
         logistic_model.fit(X_train, y_train)
 
-        predicted_prob = logistic_model.predict_proba(X_valid)
+        pred_prob = logistic_model.predict_proba(X_valid)
         log_scores.append(log_loss(y_valid, pred_prob))
         acc_scores.append(logistic_model.score(X_valid, y_valid))
 
@@ -156,22 +157,24 @@ def Logistic_Regression_score(X, y):
     # predicted = logistic_model.predict_proba(test_data)
 
 def Logistic_Regression_kaggle(test_data, X, y, listing_id):
-    logistic_model = LogisticRegression()
+    logistic_model = LogisticRegression(max_iter=10000)
     logistic_model.fit(X, y)
-    print("classes are:", logistic_model.classes_)
+   
     predicted = logistic_model.predict_proba(test_data)
 
-    print(len(predicted))
-    col_names = ['high', 'low', 'medium']
+    col_names = logistic_model.classes_
     df_predict_proba = pd.DataFrame(predicted, columns= col_names)
+    df_predict_proba= df_predict_proba.reset_index(drop=True)
 
-    print(df_predict_proba.shape)
-    df_predict_proba['listing_id'] = listing_id.astype(int)
+    listing_id= listing_id.reset_index(drop=True)
+
+    
+    df_predict_proba['listing_id'] = listing_id
     col_names = ['listing_id', 'high', 'medium', 'low']
 
     df_predict_proba = df_predict_proba[col_names]
-    print(df_predict_proba.shape)
-    df_predict_proba= df_predict_proba.dropna()
+    
+    
     df_predict_proba.to_csv('kaggle_submission_LR.csv', index=False)
 
 def SVM_score(X, y):
@@ -189,7 +192,7 @@ def SVM_score(X, y):
         svm_model = SVC(kernel='linear', probability=True, max_iter=10)
         svm_model.fit(X_train, y_train)
 
-        predicted_prob = svm_model.predict_proba(X_valid)
+        pred_prob = svm_model.predict_proba(X_valid)
         log_scores.append(log_loss(y_valid, pred_prob))
         acc_scores.append(svm_model.score(X_valid, y_valid))
         
@@ -208,39 +211,75 @@ def SVM_kaggle(test_data, X, y, listing_id):
 
     col_names = svm_model.classes_
     df_predict_proba = pd.DataFrame(predicted, columns= col_names)
+    df_predict_proba= df_predict_proba.reset_index(drop=True)
 
-    df_predict_proba['listing_id'] = listing_id.astype(int)
+    df_predict_proba['listing_id'] = listing_id
+
+    listing_id= listing_id.reset_index(drop=True)
+
+    df_predict_proba['listing_id'] = listing_id
+
     col_names = ['listing_id', 'high', 'medium', 'low']
 
-    df_predict_proba = df_predict_proba[col_names]
-    df_predict_proba= df_predict_proba.dropna()
-    df_predict_proba.to_csv('kaggle_submission_SVM.csv', index=False)
     
+
+    df_predict_proba = df_predict_proba[col_names]
+    
+    df_predict_proba.to_csv('kaggle_submission_SVM.csv', index=False)
+
+def Exploratory_Data_Analysis(data):
+
+    ##plot hour-wise listing trend and find top 5 busiest hours
+    data['created'] = pd.to_datetime(data['created']) #double check that this converts AM/PM to 24hr time
+    data['hour_created'] = data['created'].dt.hour
+    
+
+def Text_Feature_Extraction(data):
+    #extract word count
+    data['word_count'] = data['description'].str.count(' ') + 1
+
+    #extract most popular features
+    all_feature_words = sum(data.features, [])
+    feature_word_counts =  Counter(all_feature_words)
+    top_words = [word for word,cnt in feature_word_counts.most_common(70)]
+    data['common_features'] = data['features'].apply(lambda x: [word for word in x if word in top_words])
+    data.to_csv('raw_data.csv', index=False)
+    for word in top_words:
+        data[word] = data['features'].apply(lambda x:hasFeature(word,x))
+def hasFeature(word,row):
+    if word in row: 
+        return 1 
+    return 0 
 
 if __name__ == '__main__':
     data = pd.read_csv('data_after_M1.csv')
-    # data = pd.read_json(sys.argv[1])
-    test_data = pd.read_csv('test_raw_data.csv')
-    # test_data = pd.read_json(sys.argv[1])
-    test_data.to_csv('test.csv', index = False)
+    
+    test_data = pd.read_json(sys.argv[1])
+    Exploratory_Data_Analysis(test_data)
+    Text_Feature_Extraction(test_data)
+    Feature_Selection(test_data)
+    Feature_Selection(data)
+
     listing_id = test_data['listing_id']
+
     print("Listing Id shape:", listing_id.shape)
     # test_data =data.drop(data.columns[[0,3,4,5,6,7,9,11,12,14]])
-    test_data = test_data[['bathrooms','bedrooms','latitude','longitude','price','hour_created','word_count','Doorman','No Fee','Pre-War','Dining Room', 'Balcony','SIMPLEX','LOWRISE','Garage','Reduced Fee','Furnished','LAUNDRY','Hardwood','dist_dt']]
+    test_data = test_data[['bathrooms','bedrooms','latitude','longitude','price','hour_created','word_count','Doorman','No Fee','Pre-War','Dining Room', 'Balcony','SIMPLEX','LOWRISE','Garage','Reduced Fee','Furnished','LAUNDRY','Hardwood']]
+
     print("After dropping:", test_data.shape)
-    data = data.sample(n=140)
-    # X= data[['bathrooms','bedrooms','latitude','longitude','price','hour_created','word_count','Doorman','No Fee','Pre-War','Dining Room', 'Balcony','SIMPLEX','LOWRISE','Garage','Reduced Fee','Furnished','LAUNDRY','Hardwood','dist_dt']]
+    # data = data.sample(n=140)
+    X= data[['bathrooms','bedrooms','latitude','longitude','price','hour_created','word_count','Doorman','No Fee','Pre-War','Dining Room', 'Balcony','SIMPLEX','LOWRISE','Garage','Reduced Fee','Furnished','LAUNDRY','Hardwood']]
+
     y = data['interest_level']
     # Nearest_Subway(data)
-    Distance_Downtown(data)
-    X= data[['bathrooms','bedrooms','latitude','longitude','price','hour_created','word_count','Doorman','No Fee','Pre-War','Dining Room', 'Balcony','SIMPLEX','LOWRISE','Garage','Reduced Fee','Furnished','LAUNDRY','Hardwood','dist_dt']]
+    # Distance_Downtown(data)
 
-    Feature_Selection(data)
-    # Decision_tree_kaggle(test_data, X, y, listing_id)
+    
+    Decision_tree_kaggle(test_data, X, y, listing_id)
     # Logistic_Regression_kaggle(test_data, X, y, listing_id)
-    # SVM_kaggle(test_data, X, y, listing_id)
+    SVM_kaggle(test_data, X, y, listing_id)
     # data.to_csv('testtesttest.csv', index=False)
     # Decision_Tree(data, X, y)
     # Logistic_Regression_score(X, y)
-    SVM_score(X, y)
+    # SVM_score(X, y)
     # Nearest_Subway(data)
